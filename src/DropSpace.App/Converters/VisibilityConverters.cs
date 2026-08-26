c03014448aef2f778c55ef94c37157298b8125bd
Aren Vox <airanluo@gmail.com>
2026-08-27 04:25:45 +0800

    Add visibility converters referenced by App.xaml

---
+++ src/DropSpace.App/Converters/VisibilityConverters.cs
@@ -0,0 +1,42 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace DropSpace.App.Converters;
+
+public sealed class BoolToVisibilityConverter : IValueConverter
+{
+    public bool Invert { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        var visible = value is bool flag && flag;
+        if (Invert)
+        {
+            visible = !visible;
+        }
+
+        return visible ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+        => throw new NotSupportedException();
+}
+
+public sealed class NullToVisibilityConverter : IValueConverter
+{
+    public bool Invert { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        var visible = value is not null;
+        if (Invert)
+        {
+            visible = !visible;
+        }
+
+        return visible ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+        => throw new NotSupportedException();
+}
